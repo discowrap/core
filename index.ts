f5892b1b035eb93799a00871f1d8f9b80e9ae73d
Peter Dixon-Moses <peter@searchintuition.com>
2020-12-11 02:54:24 -0500

    Packaging

---
+++ index.ts
@@ -0,0 +1,9 @@
+export { Definition } from './src/lib/Definition';
+export { Engine } from './src/lib/Engine';
+export { EngineResponse, successful } from './src/lib/EngineResponse';
+export { JSONDefinition } from './src/lib/JSONDefinition';
+export { Project } from './src/lib/Project';
+export { ProjectStructure } from './src/lib/ProjectStructure';
+export { Query } from './src/lib/Query';
+export { SearchResults } from './src/lib/SearchResults';
+export { IntegrationTest } from './src/lib/IntegrationTest';
